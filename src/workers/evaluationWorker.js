ff48395b958cafd5a4c441d71fc008359f1693b2
Arunav <arunavsutar@gmail.com>
2024-08-05 01:11:19 +0530

    Added the axios request for sending the payload to the socket service.

--- src/workers/evaluationWorker.js
+++ src/workers/evaluationWorker.js
@@ -1,9 +1,19 @@
 const { Worker } = require('bullmq');
-const redisConnection = require('../config/redis.config')
+const redisConnection = require('../config/redis.config');
+const axios = require('axios');
+
 function evaluationWorker(QueueName) {
     new Worker(QueueName, async job => {
         if (job.name === 'evaluationJob') {
-            console.log(job.data);
+            try {
+                const response = await axios.post('http://localhost:5000/sendPayLoad', {
+                    userId: job.data.userId,
+                    payload: job.data
+                });
+                console.log(response);
+            } catch (error) {
+                console.log(error);
+            }
         }
     }, { connection: redisConnection });
 }
